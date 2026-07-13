00974278a4a7a08530145e285288cac83e77191f
David Fornés Martínez <davidfornesm@gmail.com>
2026-07-13 21:29:01 +0200

    implement list kind serialization and clean up number serialization

--- waggle-bencode/src/ser/serializer.rs
+++ waggle-bencode/src/ser/serializer.rs
@@ -1,87 +1,85 @@
 use serde::Serialize;
 use serde::ser;
-use serde::ser::Impossible;
-use std::fmt::Display;
+use serde::ser::{Impossible, SerializeSeq, SerializeTuple, SerializeTupleStruct};
 use std::io::Write;
-
-struct Serializer<W: Write> {
-    writer: W,
-}
 
 pub fn to_bytes<T>(value: &T) -> crate::ser::result::Result<Vec<u8>>
 where
-    T: Serialize,
+    T: ?Sized + Serialize,
 {
     let mut serializer = Serializer { writer: Vec::new() };
     value.serialize(&mut serializer)?;
     Ok(serializer.writer)
 }
 
-impl<W: Write> Serializer<W> {
-    fn write_number<D: Display>(&mut self, number: D) -> crate::ser::result::Result<()> {
-        write!(self.writer, "i{number}e")?;
-        Ok(())
-    }
-
-    fn write_bytes(&mut self, bytes: &[u8]) -> crate::ser::result::Result<()> {
-        write!(self.writer, "{}:", bytes.len())?;
-        self.writer.write_all(bytes)?;
-        Ok(())
-    }
+struct Serializer<W> {
+    writer: W,
 }
 
-impl<W: Write> ser::Serializer for &mut Serializer<W> {
+struct ListSerializer<'s, W> {
+    serializer: &'s mut Serializer<W>,
+}
+
+impl<'s, W: Write> ser::Serializer for &'s mut Serializer<W> {
     type Ok = ();
     type Error = crate::ser::error::Error;
-    type SerializeSeq = Impossible<(), Self::Error>;
-    type SerializeTuple = Impossible<(), Self::Error>;
-    type SerializeTupleStruct = Impossible<(), Self::Error>;
+    type SerializeSeq = ListSerializer<'s, W>;
+    type SerializeTuple = ListSerializer<'s, W>;
+    type SerializeTupleStruct = ListSerializer<'s, W>;
     type SerializeTupleVariant = Impossible<(), Self::Error>;
     type SerializeMap = Impossible<(), Self::Error>;
     type SerializeStruct = Impossible<(), Self::Error>;
     type SerializeStructVariant = Impossible<(), Self::Error>;
 
-    fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
+    fn serialize_bool(self, _v: bool) -> Result<Self::Ok, Self::Error> {
         Err(Self::Error::NotSupported("bool"))
     }
 
     fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
-        self.serialize_i64(i64::from(v))
+        self.write_number(v)
     }
 
     fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
-        self.serialize_i64(i64::from(v))
+        self.write_number(v)
     }
 
     fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
-        self.serialize_i64(i64::from(v))
+        self.write_number(v)
     }
 
     fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
         self.write_number(v)
     }
 
+    fn serialize_i128(self, v: i128) -> Result<Self::Ok, Self::Error> {
+        self.write_number(v)
+    }
+
     fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
-        self.serialize_u64(u64::from(v))
+        self.write_number(v)
     }
 
     fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
-        self.serialize_u64(u64::from(v))
+        self.write_number(v)
     }
 
     fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
-        self.serialize_u64(u64::from(v))
+        self.write_number(v)
     }
 
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
         self.write_number(v)
     }
 
-    fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
+    fn serialize_u128(self, v: u128) -> Result<Self::Ok, Self::Error> {
+        self.write_number(v)
+    }
+
+    fn serialize_f32(self, _v: f32) -> Result<Self::Ok, Self::Error> {
         Err(Self::Error::NotSupported("f32"))
     }
 
-    fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
+    fn serialize_f64(self, _v: f64) -> Result<Self::Ok, Self::Error> {
         Err(Self::Error::NotSupported("f64"))
     }
 
@@ -99,50 +97,50 @@ impl<W: Write> ser::Serializer for &mut Serializer<W> {
     }
 
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Err(Self::Error::NotSupported("none"))
     }
 
     fn serialize_some<T>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: ?Sized + Serialize,
     {
-        todo!()
+        value.serialize(self)
     }
 
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Err(Self::Error::NotSupported("unit"))
     }
 
-    fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
+        Err(Self::Error::NotSupported("unit_struct"))
     }
 
     fn serialize_unit_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
     fn serialize_newtype_struct<T>(
         self,
-        name: &'static str,
+        _name: &'static str,
         value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
         T: ?Sized + Serialize,
     {
-        todo!()
+        value.serialize(self)
     }
 
     fn serialize_newtype_variant<T>(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
-        value: &T,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
         T: ?Sized + Serialize,
@@ -150,51 +148,155 @@ impl<W: Write> ser::Serializer for &mut Serializer<W> {
         todo!()
     }
 
-    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        todo!()
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        let mut list_serializer = ListSerializer { serializer: self };
+        list_serializer.start()?;
+        Ok(list_serializer)
     }
 
     fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
-        todo!()
+        self.serialize_seq(Some(len))
     }
 
     fn serialize_tuple_struct(
         self,
-        name: &'static str,
+        _name: &'static str,
         len: usize,
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
-        todo!()
+        self.serialize_tuple(len)
     }
 
     fn serialize_tuple_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
-        len: usize,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
         todo!()
     }
 
-    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
         todo!()
     }
 
     fn serialize_struct(
         self,
-        name: &'static str,
-        len: usize,
+        _name: &'static str,
+        _len: usize,
     ) -> Result<Self::SerializeStruct, Self::Error> {
         todo!()
     }
 
     fn serialize_struct_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
-        len: usize,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
         todo!()
+    }
+
+    fn is_human_readable(&self) -> bool {
+        false
+    }
+}
+
+impl<W: Write> SerializeSeq for ListSerializer<'_, W> {
+    type Ok = ();
+    type Error = crate::ser::error::Error;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<(), Self::Error>
+    where
+        T: ?Sized + Serialize,
+    {
+        self.serialize_element(value)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        self.end()
+    }
+}
+
+impl<W: Write> SerializeTuple for ListSerializer<'_, W> {
+    type Ok = ();
+    type Error = crate::ser::error::Error;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<(), Self::Error>
+    where
+        T: ?Sized + Serialize,
+    {
+        self.serialize_element(value)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        self.end()
+    }
+}
+
+impl<W: Write> SerializeTupleStruct for ListSerializer<'_, W> {
+    type Ok = ();
+    type Error = crate::ser::error::Error;
+
+    fn serialize_field<T>(&mut self, value: &T) -> Result<(), Self::Error>
+    where
+        T: ?Sized + Serialize,
+    {
+        self.serialize_element(value)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        self.end()
+    }
+}
+
+impl<W: Write> Serializer<W> {
+    fn write_number<I: itoa::Integer>(&mut self, number: I) -> crate::ser::result::Result<()> {
+        self.write_number_delimiter()?;
+        let mut buffer = itoa::Buffer::new();
+        self.writer.write_all(buffer.format(number).as_bytes())?;
+        self.write_end()?;
+        Ok(())
+    }
+
+    fn write_bytes(&mut self, bytes: &[u8]) -> crate::ser::result::Result<()> {
+        let mut buffer = itoa::Buffer::new();
+        self.writer
+            .write_all(buffer.format(bytes.len()).as_bytes())?;
+        self.writer.write_all(b":")?;
+        self.writer.write_all(bytes)?;
+        Ok(())
+    }
+
+    fn write_number_delimiter(&mut self) -> crate::ser::result::Result<()> {
+        self.writer.write_all(b"i")?;
+        Ok(())
+    }
+
+    fn write_list_delimiter(&mut self) -> crate::ser::result::Result<()> {
+        self.writer.write_all(b"l")?;
+        Ok(())
+    }
+
+    fn write_end(&mut self) -> crate::ser::result::Result<()> {
+        self.writer.write_all(b"e")?;
+        Ok(())
+    }
+}
+
+impl<W: Write> ListSerializer<'_, W> {
+    fn start(&mut self) -> Result<(), crate::ser::error::Error> {
+        self.serializer.write_list_delimiter()
+    }
+    fn serialize_element<T>(&mut self, value: &T) -> Result<(), crate::ser::error::Error>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut *self.serializer)
+    }
+
+    fn end(self) -> Result<(), crate::ser::error::Error> {
+        self.serializer.write_end()
     }
 }
--- waggle-bencode/src/ser/tests.rs
+++ waggle-bencode/src/ser/tests.rs
@@ -1,4 +1,5 @@
 use crate::to_bytes;
+use serde::Serialize;
 
 #[test]
 fn serialize_positive_number() {
@@ -37,5 +38,58 @@ fn serialize_empty_bytes() {
     let bytes = "";
     let expected = b"0:";
     let serialized = to_bytes(&bytes).unwrap();
+    assert_eq!(serialized, expected)
+}
+
+#[test]
+fn serialize_list_of_integers() {
+    let list = vec![1, 2, 3];
+    let expected = b"li1ei2ei3ee";
+    let serialized = to_bytes(&list).unwrap();
+    assert_eq!(serialized, expected)
+}
+
+#[test]
+fn serialize_list_of_bytes() {
+    let list = vec!["hi", "hey", "hello"];
+    let expected = b"l2:hi3:hey5:helloe";
+    let serialized = to_bytes(&list).unwrap();
+    assert_eq!(serialized, expected)
+}
+
+#[test]
+fn serialize_empty_list() {
+    let list: Vec<i32> = Vec::new();
+    let expected = b"le";
+    let serialized = to_bytes(&list).unwrap();
+    assert_eq!(serialized, expected)
+}
+
+#[test]
+fn serialize_tuple_of_integer_and_bytes() {
+    let tuple = (42, "hello");
+    let expected = b"li42e5:helloe";
+    let serialized = to_bytes(&tuple).unwrap();
+    assert_eq!(serialized, expected)
+}
+
+#[derive(Serialize)]
+struct TupleStruct(i32, i32);
+
+#[test]
+fn serialize_tuple_struct() {
+    let value = TupleStruct(3, 12);
+    let expected = b"li3ei12ee";
+    let serialized = to_bytes(&value).unwrap();
+    assert_eq!(serialized, expected)
+}
+
+#[derive(Serialize)]
+struct EmptyTupleStruct();
+#[test]
+fn serialize_empty_tuple_struct() {
+    let value = EmptyTupleStruct();
+    let expected = b"le";
+    let serialized = to_bytes(&value).unwrap();
     assert_eq!(serialized, expected)
 }
